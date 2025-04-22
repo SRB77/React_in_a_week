1bb90231053ba7a9396f84baf503e6f1c85036a7
SRB77 <soumyach144@gmail.com>
2025-04-22 14:17:22 +0530

    create basic react app using CDN

---
+++ ep2_igniting/index.js
@@ -0,0 +1,26 @@
+// * Let's create this with help of React
+/* 
+    <div class="parent">
+      <div class="class1">
+        <h1 id="heading1">Hello i am heading 1</h1>
+        <h2 id="heading2">Hello i am heading 2</h2>
+      </div>
+      <div class="class2">
+        <h1 id="heading3">Hello i am heading 3</h1>
+        <h2 id="heading4">Hello i am heading 4</h2>
+      </div>
+    </div> 
+ */
+
+const parent = React.createElement("div", { className: "parent" }, [
+  React.createElement("div", { className: "class1" }, [
+    React.createElement("h1", { id: "heading1" }, "Hello i am heading1"),
+    React.createElement("h1", { id: "heading2" }, "Hello i am heading2"),
+  ]),
+  React.createElement("div", { className: "class1" }, [
+    React.createElement("h1", { id: "heading1" }, "Hello i am heading3 "),
+    React.createElement("h1", { id: "heading2" }, "Hello i am heading4"),
+  ]),
+]);
+const root = ReactDOM.createRoot(document.querySelector(".main"));
+root.render(parent)
